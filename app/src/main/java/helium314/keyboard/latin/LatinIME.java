2654a22aa50f60de38c79df6cd1b32be06314a97
devycarol <[email protected]>
2026-08-22 12:33:51 -0600

    another note

--- app/src/main/java/helium314/keyboard/latin/LatinIME.java
+++ app/src/main/java/helium314/keyboard/latin/LatinIME.java
@@ -1137,6 +1137,7 @@ public class LatinIME extends InputMethodService implements
     @Override
     public void hideWindow() {
         Log.i(TAG, "hideWindow");
+        // TODO LOL: stop listening when the screen locks
         mVoiceTranscriber.stopListening();
         if (hasSuggestionStripView() && mSettings.getCurrent().mToolbarMode == ToolbarMode.EXPANDABLE)
             mSuggestionStripView.setToolbarVisibility(false);
